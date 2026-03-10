17bdb528517bb3a904bb89374c2e36f6c957adf8
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-03-10 23:00:48 +0700

    JS 2

---
+++ app/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'slug',
+        'category_id',
+        'color',
+        'image',
+        'body',
+        'tags',
+        'published',
+        'published_at',
+    ];
+    protected $casts = [
+        'tags' => 'array',
+        'published' => 'boolean',
+        'published_at' => 'date',
+    ];
+}
