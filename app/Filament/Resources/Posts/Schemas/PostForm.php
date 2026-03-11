0d871f938cd3acae54dc91ea93fa79d49f15f735
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-03-11 08:46:05 +0700

    JS 5

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -12,6 +12,7 @@ use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Group;
 use Filament\Support\Icons\Heroicon;
 
 class PostForm
@@ -24,28 +25,35 @@ class PostForm
                     ->description("Fill in the details of the post")
                     ->icon('heroicon-o-document-text')
                     ->schema([
-                        TextInput::make('title')->required(),
-                        TextInput::make('slug')->required(),
-                        Select::make('category_id')
-                            ->relationship('category', 'name')
-                            ->preload()
-                            ->searchable(),
-                        ColorPicker::make('color'),
+                        Group::make([
+                            TextInput::make('title')->required(),
+                            TextInput::make('slug')->required(),
+                            Select::make('category_id')
+                                ->relationship('category', 'name')
+                                ->preload()
+                                ->searchable(),
+                            ColorPicker::make('color'),
+                        ])->columns(2),
+
                         MarkdownEditor::make('body')
                             ->label('Content'),
-                    ]),
-                Section::make('Image Upload')
-                    ->schema([
-                        FileUpload::make('image')
-                            ->disk('public')
-                            ->directory('posts'),
-                    ]),
-                Section::make('Meta')
-                    ->schema([
-                        TagsInput::make('tags'),
-                        Checkbox::make('published'),
-                        DateTimePicker::make('published_at'),
-                    ]),
-            ])->columns(2);
+                    ])->columnSpan(2),
+                Group::make([
+                    Section::make('Image Upload')
+                        ->icon('heroicon-o-photo')
+                        ->schema([
+                            FileUpload::make('image')
+                                ->disk('public')
+                                ->directory('posts'),
+                        ]),
+                    Section::make('Meta Information')
+                        ->icon('heroicon-o-cog-6-tooth')
+                        ->schema([
+                            TagsInput::make('tags'),
+                            Checkbox::make('published'),
+                            DateTimePicker::make('published_at'),
+                        ])->columns(2),
+                ])->columnSpan(1),
+            ])->columns(3);
     }
 }
