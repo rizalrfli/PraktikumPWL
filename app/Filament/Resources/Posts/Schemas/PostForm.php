df20944a81838afc403575465933ef0872322462
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-03-25 16:27:41 +0700

    JS 6

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -26,10 +26,28 @@ class PostForm
                     ->icon('heroicon-o-document-text')
                     ->schema([
                         Group::make([
-                            TextInput::make('title')->required(),
-                            TextInput::make('slug')->required(),
+                            TextInput::make("title")
+                                ->required()
+                                ->minLength(5)
+                                ->validationMessages([
+                                    'required' => 'Judul wajib diisi.',
+                                    'min' => 'Judul minimal 5 karakter.'
+                                ]),
+                            TextInput::make('slug')
+                                ->required()
+                                ->minLength(3)
+                                ->unique(ignoreRecord: true)
+                                ->validationMessages([
+                                    "unique" => "Slug harus unique.",
+                                    "required" => "Slug wajib diisi.",
+                                    "min" => "Slug minimal 3 karakter."
+                                ]),
                             Select::make('category_id')
                                 ->relationship('category', 'name')
+                                ->required()
+                                ->validationMessages([
+                                    'required' => 'Kategori wajib dipilih.'
+                                ])
                                 ->preload()
                                 ->searchable(),
                             ColorPicker::make('color'),
@@ -43,6 +61,7 @@ class PostForm
                         ->icon('heroicon-o-photo')
                         ->schema([
                             FileUpload::make('image')
+                                ->required()
                                 ->disk('public')
                                 ->directory('posts'),
                         ]),
