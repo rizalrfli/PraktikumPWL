ff7a629fe46b15cd6a9f77e405e7b87f770bed8f
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2026-03-26 16:51:29 +0700

    JS 7

---
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Resources\Products\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Table;
+
+class ProductsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('sku'),
+                TextColumn::make('price'),
+                TextColumn::make('stock'),
+                ImageColumn::make('image')
+                    ->disk('public'),
+                IconColumn::make('is_active')
+                    ->boolean(),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
